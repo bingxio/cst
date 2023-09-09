67bfc348978a85228cea96133ffba11421bd157f
bingxio <bingxio@qq.com>
2023-09-09 16:07:12 +0800

    update.

--- test.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-from subprocess import run
-
-x, y, m = randint(1, 8), randint(1, 8), randint(1, 6)
-print("top=%d, bottom=%d, move=%d" % (x, y, m))
-run(["python3", "mh.py", "%d%d%d" % (x, y, m)])
